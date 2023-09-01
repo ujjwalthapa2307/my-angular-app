ce8770acea1694c492fbd13ef6b2a95790e539a9
ujjwalthapa2307 <ujjwalthapa2307@gmail.com>
2023-09-01 16:07:47 -0400

    media component added

---
+++ src/app/media-item/media-item.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-media-item',
+  templateUrl: './media-item.component.html',
+  styleUrls: ['./media-item.component.css']
+})
+export class MediaItemComponent {
+
+}
