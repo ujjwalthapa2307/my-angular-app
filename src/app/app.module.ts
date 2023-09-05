2f1c95f3fc6cba635b364d8cde3c22614f6dcde5
ujjwalthapa2307 <ujjwalthapa2307@gmail.com>
2023-09-05 19:05:06 -0400

    isfavorite diectives added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MediaItemComponent } from './media-item/media-item.component';
 import { MediaItemListComponent } from './media-item-list/media-item-list.component';
+import { FavoriteDirective } from './favorite.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
     MediaItemComponent,
-    MediaItemListComponent
+    MediaItemListComponent,
+    FavoriteDirective
   ],
   imports: [
     BrowserModule,
